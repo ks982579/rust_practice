61038d2514e42689b2ed64072c48e39890c6f981
Kevin Sullivan <kevinsullivan@ksulldev.space>
2023-06-27 08:07:43 +0100

    First pass at obfuscate

--- password_protector/src/main.rs
+++ password_protector/src/main.rs
@@ -22,6 +22,25 @@ struct Limits {
     lower: i16,
 }
 
+// Can make functions and work with those
+// Or turn up complexity and work with implementations!
+
+fn obfuscate(password: String) -> String {
+    // takes ownership to delete existence
+    let mut new_bytes: Vec<u8> = Vec::new();
+    for byte in password.into_bytes() {
+        println!("{} -> {}", &byte, String::from_utf8(vec![byte]).unwrap());
+        new_bytes.push(byte + 1);
+        println!("{} -> {}", &byte+1, String::from_utf8(vec![byte+1]).unwrap());
+    }
+    // println!("{:?}", &password.as_bytes());
+    String::from_utf8(new_bytes).unwrap()
+}
+
+fn unobfuscate() {
+    println!("Unblurring...")
+}
+
 fn fetch_args() {
     let action: Option<String> = std::env::args().nth(1);
 }
