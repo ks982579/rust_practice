3bb6b9b35028d9ac0026ad9b43cc57fce0e39211
Kevin Sullivan <kevinsullivan@ksulldev.space>
2024-03-19 07:21:10 +0000

    Finished terrifying Non-Priority Queue example

--- basics/src/lists.rs
+++ basics/src/lists.rs
@@ -1,3 +1,6 @@
+use std::cell::{RefCell, RefMut};
+use std::rc::Rc;
+
 #[derive(Debug, PartialEq, PartialOrd, Clone)]
 pub struct StackNode<T> {
     value: T,
@@ -54,22 +57,62 @@ impl<T> Stack<T> {
 #[derive(Debug, Clone, PartialEq, PartialOrd)]
 pub struct NonPriorityQueueNode<T> {
     pub value: T,
+    pub next: Option<Rc<RefCell<NonPriorityQueueNode<T>>>>,
+}
+
+impl<T> NonPriorityQueueNode<T> {
+    fn new(val: T) -> Self {
+        Self {
+            value: val,
+            next: None,
+        }
+    }
 }
 
 #[derive(Debug, Clone, PartialEq, PartialOrd)]
 pub struct NonPriorityQueue<T> {
-    pub queue: Option<Box<NonPriorityQueueNode<T>>>,
+    pub head: Option<Rc<RefCell<NonPriorityQueueNode<T>>>>,
+    pub tail: Option<Rc<RefCell<NonPriorityQueueNode<T>>>>,
 }
 
-impl<T> NonPriorityQueue<T> {
+impl<'a, T> NonPriorityQueue<T>
+where
+    T: Clone,
+{
     pub fn new() -> Self {
-        NonPriorityQueue { queue: None }
+        NonPriorityQueue {
+            head: None,
+            tail: None,
+        }
     }
-    pub fn enqueue() {
-        todo!();
+    pub fn enqueue(&mut self, val: T) {
+        let new_node = Rc::new(RefCell::new(NonPriorityQueueNode::new(val)));
+        match self.tail.take() {
+            None => {
+                self.tail = Some(Rc::clone(&new_node));
+                self.head = Some(Rc::clone(&new_node));
+            }
+            Some(ref_cnt) => {
+                let mut end_elm: RefMut<'_, NonPriorityQueueNode<T>> = (*ref_cnt).borrow_mut();
+                (*end_elm).next = Some(Rc::clone(&new_node));
+                self.tail = Some(Rc::clone(&new_node));
+            }
+        }
     }
-    pub fn dequeue() {
-        todo!();
+    pub fn dequeue(&mut self) -> Option<T> {
+        match self.head.take() {
+            None => {
+                return None;
+            }
+            Some(old_head) => {
+                self.head = (*old_head).borrow_mut().next.take();
+                if let None = self.head {
+                    self.tail = None;
+                }
+                let rv: T = (*old_head).borrow().value.clone();
+                Some(rv)
+            }
+        }
     }
 }
 
@@ -130,11 +173,35 @@ mod tests {
     #[test]
     fn test_make_non_priority_queue() {
         let actual: NonPriorityQueue<i8> = NonPriorityQueue::new();
-        let excpected: NonPriorityQueue<i8> = NonPriorityQueue { queue: None };
-        let fails_val: NonPriorityQueue<i8> = NonPriorityQueue {
-            queue: Some(Box::new(NonPriorityQueueNode::<i8> { value: 5 })),
+        let excpected: NonPriorityQueue<i8> = NonPriorityQueue {
+            head: None,
+            tail: None,
         };
         assert!(actual == excpected);
-        assert_ne!(actual, fails_val);
+    }
+    #[test]
+    fn test_npq_enqueue() {
+        let val: i32 = 42;
+        let new_node = Rc::new(RefCell::new(NonPriorityQueueNode::new(val)));
+        let mut actual: NonPriorityQueue<i32> = NonPriorityQueue::new();
+        actual.enqueue(val);
+        let expected: NonPriorityQueue<i32> = NonPriorityQueue {
+            head: Some(Rc::clone(&new_node)),
+            tail: Some(Rc::clone(&new_node)),
+        };
+        assert_eq!(actual, expected);
+    }
+    #[test]
+    fn test_npq_dequeue() {
+        let mut actual: NonPriorityQueue<i32> = NonPriorityQueue::new();
+        actual.enqueue(42);
+        let actual_value = actual.dequeue().unwrap();
+        let expected_val: i32 = 42;
+        let expected_q: NonPriorityQueue<i32> = NonPriorityQueue {
+            head: None,
+            tail: None,
+        };
+        assert_eq!(actual_value, expected_val);
+        assert_eq!(actual, expected_q);
     }
 }
